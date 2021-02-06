ddd7865bcf24efd244daea93528bc5f12e85109b
Tony Lockhart <tlockhart6@gmail.com>
2021-02-06 13:58:54 -0500

    initial commit

---
+++ hash.ts
@@ -0,0 +1,48 @@
+interface ObjectType {
+    [key: string]: string | number
+}
+
+declare interface objectContructor {
+    assign(...objects: Object[]): Object;
+}
+
+class Hash{
+    hash!: {[key: string]: string | number};
+    key!: string;
+    value!: string;
+
+    constructor(objects: ObjectType) {
+        const keys = Object.keys(objects);
+        console.log("keys:", keys);
+        keys.forEach((key: string) => {
+            this.push(key, objects[`${key}`]);
+        })
+        console.log("Hash:", this.hash);
+    }
+    // Take new key value pair and assing to hash
+    push(key: string, value: string | number) {
+        let target: {[string: string]: string | number} = {};
+        let source: {[string: string]: string | number} = {};
+        if (key === "step" || key === "min" || key === "max") {
+            // Store new value to source
+            source = this.assignValue(value, key);
+            // copy ne source to target and assign target to hash
+            console.log("source:", source);
+            this.hash = {...this.hash,...source};
+        }
+        return this.hash;
+    }
+    // Assigns hash key values
+    assignValue(value: string | number, key: string): {[string: string]: string} {
+        const sourceHash: {[string: string]: string} = {};
+        sourceHash[key] = value.toString();
+        return sourceHash;
+    }
+}
+
+const myObject = {
+        step: 0.1,
+        min: 0,
+        max: 100
+    };
+const myHash = new Hash(myObject);
